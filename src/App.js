2c95b1bcc08d9b6a2f04900f23ea63bd83085791
ganeshkunwar <ganeshkunwar1112@gmail.com>
2024-05-16 16:19:30 +0545

    App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="body">
+      <header>
+        <h1 class="logo">logo</h1>
+        <navbar class="navigation">
+          <a href="Home"> Home</a>
+          <a href="About">About</a>
+          <a href="Service">Service</a>
+          <a href="contact">Contact</a>
+          <button class="btnLogin-popup">Login</button>
+        </navbar>
       </header>
+      <div className="wrapper">
+        <div className="form-box login">
+          <h2>Login</h2>
+          <form action="#"></form>
+          <div className="input-box">
+            <span className="icon"></span>
+            <input type="email" required />
+            < > Email</>
+          </div>
+          <div className="input-box">
+            <span className="icon"></span>
+            <input type="Password" required/>
+            <label> Password</label>
+          </div>
+          
+          <label><input type="checkbox"/>Remember me </label>
+          <a href="/">Forget Password?</a>
+        </div>
+        <button type="submit" className="btn">Submit</button>
+      </div>
     </div>
   );
 }
